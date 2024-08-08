5f7fd54c5c6e4b24afa32d4c4e3a992843eeca3f
Lina <linaoeu567556@gmail.com>
2024-08-08 20:00:05 +0700

    Practice about Values, Types, and Operators

---
+++ variable.js
@@ -0,0 +1,57 @@
+// Value Types and Operator
+console.log(5+1)
+console.log(2.5e2)
+// Arithmetic
+console.log(100 + 4 * 11)
+console.log((100 + 4) * 11)
+// String
+console.log("Hello")
+console.log('My name is')
+console.log(`Lina`)
+// \n
+console.log("This is the first line\nAnd this is the second")
+console.log("A newline character is written like \"\\n\".")
+concatenate
+console.log("Hello "+"my "+"name "+"is "+"Lina.")
+
+const a = 10;
+const b = 20;
+const c = a+b
+console.log(`half of c is ${c/2}`)
+
+// Unary operators
+console.log(typeof 4.5)
+console.log(typeof "x")
+console.log(typeof 3)
+// Boolean values
+const x=3;
+const y=7;
+console.log(x>y)
+console.log(x<y)
+console.log(x!=y)
+console.log("Oeu" < "Lina")
+// NaN
+console.log(NaN == NaN)
+// Logical operators
+console.log(true && false)
+console.log(1 && 0)
+console.log(false || true)
+console.log(1 || 0)
+console.log(1 + 1 == 2 && 10 * 10 > 50)
+// ternary
+console.log(3>4 ? 1 : 2);
+console.log(false ? 1 : 2);
+// Empty values
+console.log(8 * null)
+console.log("5" - 1)
+console.log("5" + 1)
+console.log("five" * 2)
+console.log(false == 0)
+console.log(null || "Lina")
+console.log("Oeu" || "Lina")
+console.log(0 || 100)
+console.log(0 ?? 100)
+console.log(null ?? 100)
+
+
+
